aa8d916c0e7bf01e24ddf86f51f4cbd0441fe868
tatianavidela <tatianavidelaliz@gmail.com>
2021-08-24 15:39:33 +0200

    REST API added
    Added REST API to product list pase and specific product page. Need to add/ bag page

--- js/products.js
+++ js/products.js
@@ -1,57 +1,22 @@
-import { productArray } from "./constants/productList.js";
-const productsContainer = document.querySelector(".products");
-const bag = document.querySelector(".bag");
-const bagList = document.querySelector(".bag-list");
-const totalContainer = document.querySelector(".total-bag");
+const baseUrl = "https://tatianavidela.online/wp-json/wc/store/products";
+const productContainer = document.querySelector(".products")
 
-const minimizeButton = document.querySelector(".minimize-button")
+async function getProducts(url){
+    const response = await fetch(url);
+    const products = await response.json();
 
+    console.log(products)
 
-let bagArray= [];
-
-productArray.forEach(function(product){
-    productsContainer.innerHTML += 
-    `
-    <div class="product">
-        <h3><a href="yellowclassic.html">${product.name}</a></h3>
-        <a href="yellowclassic.html"> <img src="${product.image}" alt="${product.name}"> </a>
-        <p>${product.description}</p>
-        <div class="product-price">Price: ${product.price} NOK</div>
-        <button class="product-button" data-product="${product.id}">Add to bag</button>
-    </div>
-    `
-});
-
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach(function(button){
-    button.onclick= function(event) {
-        const itemToAdd = productArray.find(item=> item.id === event.target.dataset.product);
-        bagArray.push(itemToAdd);
-        showBag(bagArray)
-        localStorage.setItem("bagList",JSON.stringify(bagArray));  
-    }
-})
-
-function showBag(bagItems){
-    bag.style.display = "block";
-    bagList.innerHTML= "";
-    let total= 0;
-    bagItems.forEach(function(bagElement){
-        total += bagElement.price;
-        bagList.innerHTML += 
-        `<div class="bag-item">
-        <h4>${bagElement.name}</h4>
-        <img src="${bagElement.image}" alt="${bagElement.name}" class="bag-image" />
-        </div>
-        `
+    products.forEach(function(product){
+        productContainer.innerHTML += `
+        <div class= "product">
+            <a href="specific-product.html?id=${product.id}">
+            <h3>${product.name}</h3>
+            <img src="${product.images[0].src}" />
+            </a> 
+            <p><button class="product-button" type="button">Add to bag</button>${product.price_html}</p>
+        </div>`
     })
-    totalContainer.innerHTML = `Total: ${total}`;
-
 }
 
-function hideBag(){
-    bag.style.display="none"
-}
-
-minimizeButton.addEventListener("click",hideBag)
+getProducts(baseUrl);
---
+++ js/specific-product.js
@@ -0,0 +1,41 @@
+const specificProductContainer = document.querySelector(".specific-product-container")
+
+const queryString = document.location.search;
+
+console.log (queryString);
+
+const params = new URLSearchParams(queryString);
+
+console.log(params);
+
+const id = params.get("id")
+
+console.log(id)
+
+const url = "https://tatianavidela.online/wp-json/wc/store/products/" + id;
+
+async function fetchProduct() {
+    const response = await fetch(url);
+    const specificProduct = await response.json();
+
+    createHtml(specificProduct);
+}
+
+fetchProduct();
+
+function createHtml(specificProduct) {
+    specificProductContainer.innerHTML =
+    `
+    <div>
+        <h2>${specificProduct.name}</h2>
+        <p>${specificProduct.description}</p>
+        <p>${specificProduct.price_html}</p>
+        <div class="specific-product_cta">
+            <button type="button">Add to bag</button>
+        </div>
+    </div>
+    <div>
+        <img src="${specificProduct.images[0].src}" />
+    </div>
+    `
+}
